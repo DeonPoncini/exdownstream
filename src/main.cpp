21af38e4de3a40ae42d9a4f57279211ace99a299
Deon Poncini <dex1337@gmail.com>
2014-11-29 18:29:44 -0800

    Consume upstream libraries
    Using the build system, we can now easily link against other libraries

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
 
+#include <upstream1/upstream1.h>
+#include <upstream2/upstream2.h>
+
 int main(int argc, char* argv[])
 {
-    std::cout << "Hello, World!\n";
+    std::cout << "Hello from upstream packages " <<
+        upstream1::upstream_version() << " and " <<
+        upstream2::upstream_version() << std::endl;
     return 0;
 }
 
